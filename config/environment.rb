9ab1f380c493a0cbc3c5f358deb1a5e9e62754b2
Matt Daniels <maciej.wiqira@gmail.com>
2017-06-14 15:12:52 +0000

    Added mailer settings to environment file - fixed minor spelling bug

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,12 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.smtp_settigs = {
+ActionMailer::Base.smtp_settings = {
     :port       => ENV['MAILGUN_SMTP_PORT'],
     :address    => ENV['MAILGUN_SMTP_SERVER'],
     :user_name  => ENV['MAILGUN_SMTP_LOGIN'],
     :password   => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain     => 'yourapp.heroku.com',
+    :domain     => 'ancient-lowlands-59305.herokuapp.com',
     :authentication => :plain,
 }
 ActionMailer::Base.deliviery_method = :smtp
